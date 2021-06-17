0e39cefacd817e53cd93c6cc10381ae4293d2e6c
yyj <yujie8@leju.com>
2021-06-17 15:17:42 +0800

    deploy

---
+++ precache-manifest.2d2df04a91ce635f75d7dd0ce28a3c30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32982775c632c6e325d2",
+    "url": "/morney-test-yyj/css/app.e1253803.css"
+  },
+  {
+    "revision": "0d6daf8c996e0840abd2aca41cba2069",
+    "url": "/morney-test-yyj/index.html"
+  },
+  {
+    "revision": "32982775c632c6e325d2",
+    "url": "/morney-test-yyj/js/app.25b43250.js"
+  },
+  {
+    "revision": "d6faeee4e970e88f8299",
+    "url": "/morney-test-yyj/js/chunk-vendors.40cfadc0.js"
+  },
+  {
+    "revision": "34fa58f264e125e51ff260d77309bdbf",
+    "url": "/morney-test-yyj/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/morney-test-yyj/robots.txt"
+  }
+]);
